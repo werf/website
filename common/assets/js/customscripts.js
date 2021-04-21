1fff0d7568de1490f5ee0c6c09569f9ae3d24b2f
Artem Kladov <6360800+z9r5@users.noreply.github.com>
2021-04-21 13:00:03 +0300

    [web] Update layouts (#88)

--- common/assets/js/customscripts.js
+++ common/assets/js/customscripts.js
@@ -117,3 +117,15 @@ $(document).ready(function () {
     }
   });
 });
+
+
+// details
+$( document ).ready(function() {
+  $('.details__summary').on('click tap', function() {
+    $(this).closest('.details').toggleClass('active');
+  });
+
+  $('.open-content_button').on('click tap', function() {
+    $(this).closest('.open-content_container').toggleClass('active');
+  });
+});
--- common/assets/js/details.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  $('.details__summary').on('click tap', function() {
-    $(this).closest('.details').toggleClass('active');
-  });
-
-  $('.open-content_button').on('click tap', function() {
-    $(this).closest('.open-content_container').toggleClass('active');
-  });
-});
