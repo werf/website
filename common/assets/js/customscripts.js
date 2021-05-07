69be7d9792e2950a501b196bd1b7b815b688606d
Artem Kladov <6360800+z9r5@users.noreply.github.com>
2021-05-07 11:33:44 +0300

    [web] Fix details style (#90)

--- common/assets/js/customscripts.js
+++ common/assets/js/customscripts.js
@@ -121,10 +121,6 @@ $(document).ready(function () {
 
 // details
 $( document ).ready(function() {
-  $('.details__summary').on('click tap', function() {
-    $(this).closest('.details').toggleClass('active');
-  });
-
   $('.open-content_button').on('click tap', function() {
     $(this).closest('.open-content_container').toggleClass('active');
   });
