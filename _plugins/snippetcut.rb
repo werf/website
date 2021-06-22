791c5db619380ee35dca12215c6736958dacb556
Ilya Lesikov <ilya@lesikov.com>
2021-06-22 18:07:24 +0300

    [plugins] fix, refactor snippetcut
    Fix newlines when copying; allow passing of variables or templates as
    arguments, not just plain text.

--- _plugins/snippetcut.rb
+++ _plugins/snippetcut.rb
@@ -1,9 +1,11 @@
+require "liquid"
+
 module Jekyll
   module SnippetCut
     class SnippetCutTag < Liquid::Block
       @@DEFAULTS = {
-          :name => 'myfile.yaml',
-          :url => '/asdasda/myfile.yaml',
+          :name => 'changeme.yaml',
+          :url => '/changeme.yaml',
           :limited => false
       }
 
@@ -11,13 +13,22 @@ module Jekyll
         return @@DEFAULTS
       end
 
-      def initialize(tag_name, markup, tokens)
+      def initialize(tag_name, raw_markup, tokens)
         super
-
+        @raw_markup = raw_markup
         @config = {}
+      end
+
+      def render(context)
+        @markup = Liquid::Template
+          .parse(@raw_markup)
+          .render(context)
+          .gsub(%r!\\\{\\\{|\\\{\\%!, '\{\{' => "{{", '\{\%' => "{%")
+          .strip
+
         override_config(@@DEFAULTS)
 
-        params = markup.scan /([a-z]+)\=\"(.+?)\"/
+        params = @markup.scan /([a-z]+)\=\"(.+?)\"/
         if params.size > 0
           config = {}
           params.each do |param|
@@ -25,35 +36,35 @@ module Jekyll
           end
           override_config(config)
         end
-      end
 
-      def override_config(config)
-        config.each{ |key,value| @config[key] = value }
-      end
-
-      def render(context)
         content = super
-
         rendered_content = Jekyll::Converters::Markdown::KramdownParser.new(Jekyll.configuration()).convert(content)
 
         %Q(
 <div class="snippetcut#{@config[:limited] ? ' snippetcut_limited' : ''}" data-snippetcut>
-<div class="snippetcut__title">
-  #{if (@config[:url]!='#') then "<a href=\""+@config[:url]+"\" target=\"_blank\" class=\"snippetcut__title-name\" data-snippetcut-name>" else "<span class=\"snippetcut__title-name-text\">" end}
-  #{@config[:name]}
-  #{if (@config[:url]!='#') then "</a>" else "</span>" end}
+<div class="snippetcut__title">#{if (@config[:url]!='#') then "<a href=\""+@config[:url]+"\" target=\"_blank\" class=\"snippetcut__title-name\" data-snippetcut-name>" else "<span class=\"snippetcut__title-name-text\">" end}#{@config[:name]}#{if (@config[:url]!='#') then "</a>" else "</span>" end}
 <a href="javascript:void(0)" class="snippetcut__title-btn" data-snippetcut-btn-name>копировать имя</a>
 <a href="javascript:void(0)" class="snippetcut__title-btn" data-snippetcut-btn-text>копировать текст</a>
 </div>
 <div class="highlight">
 #{rendered_content}
 </div>
-<div class="snippetcut__raw" data-snippetcut-text>#{CGI::escapeHTML(content.strip.gsub(%r!^```[a-zA-Z]*!,''))}</div>
+<div class="snippetcut__raw" data-snippetcut-text>#{CGI::escapeHTML(remove_excessive_newlines(content.strip.gsub(%r!^```[a-zA-Z0-9]*!,'')))}</div>
 </div>
         )
-  end
-        end
-        end
-        end
+      end
 
-        Liquid::Template.register_tag('snippetcut', Jekyll::SnippetCut::SnippetCutTag)
+      private
+
+      def override_config(config)
+        config.each{ |key,value| @config[key] = value }
+      end
+
+      def remove_excessive_newlines(text)
+        return text.sub(/^(\s*\R)*/, "").rstrip()
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('snippetcut', Jekyll::SnippetCut::SnippetCutTag)
