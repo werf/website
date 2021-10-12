f90411fb011945f2f50b5831814257f84b98c344
Pavel Golovin <golovinps@yandex.ru>
2021-10-12 12:54:50 +0300

    080_config init

---
+++ examples/laravel/080_configuration/app/Http/Controllers/TalkerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Talker;
+use Illuminate\Http\Request;
+
+class TalkerController extends Controller
+{
+    public function say(): string
+    {
+        /** @var Talker $talker */
+        $talker = Talker::first();
+
+        if (!$talker) {
+            return "I have nothing to say";
+        }
+
+        return $talker->answer . ", " . $talker->name . "!";
+    }
+
+    public function remember(Request $request): string
+    {
+        if (!$request->has('answer')) {
+            return "You forgot the answer :(";
+        }
+
+        if (!$request->has('name')) {
+            return "You forgot the name :(";
+        }
+
+        /** @var Talker $talker */
+        $talker = Talker::firstOrNew();
+        $talker->answer = $request->input('answer');
+        $talker->name = $request->input('name');
+        $talker->save();
+
+        return "Got it.";
+    }
+}
