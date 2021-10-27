d1dd028fef9a4e14df3b20b20a048ba36b3f5e34
Pavel Golovin <golovinps@yandex.ru>
2021-10-27 17:40:35 +0300

    groom 010_basic_app

--- examples/laravel/010_basic_app/app/Http/Controllers/PingController.php
+++ examples/laravel/010_basic_app/app/Http/Controllers/PingController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+
 class PingController extends Controller
 {
     public function ping(): string
     {
+        Log::info('Ping request.');
         return 'pong';
     }
 }
