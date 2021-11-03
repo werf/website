4863d37e7c1aec3ed7017269954a3fd01ca5e6de
Pavel Golovin <golovinps@yandex.ru>
2021-11-03 15:37:40 +0300

    groom 030_assets

--- examples/laravel/030_assets/app/Exceptions/Handler.php
+++ examples/laravel/030_assets/app/Exceptions/Handler.php
@@ -17,17 +17,6 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
