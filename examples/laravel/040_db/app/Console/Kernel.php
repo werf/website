42be6907127de7a57af9ad524503f1d19c117f9d
Pavel Golovin <golovinps@yandex.ru>
2021-11-03 17:13:07 +0300

    groom 040_db

--- examples/laravel/040_db/app/Console/Kernel.php
+++ examples/laravel/040_db/app/Console/Kernel.php
@@ -15,27 +15,4 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
     ];
-
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
-    }
 }
--- examples/laravel/040_db/routes/web.php
+++ examples/laravel/040_db/routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/ping', [PingController::class, 'ping']);
 
 Route::get('/image', function () {
