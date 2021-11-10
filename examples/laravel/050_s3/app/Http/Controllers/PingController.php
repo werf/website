212073d8d0d5be322e948e0eb9962414a18381e3
Pavel Golovin <golovinps@yandex.ru>
2021-11-10 13:49:33 +0300

    groom 050_s3

--- examples/laravel/050_s3/app/Http/Controllers/PingController.php
+++ examples/laravel/050_s3/app/Http/Controllers/PingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
 
 class PingController extends Controller
--- examples/laravel/050_s3/app/Http/Controllers/TalkerController.php
+++ examples/laravel/050_s3/app/Http/Controllers/TalkerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Talker;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class TalkerController extends Controller
 {
