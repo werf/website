63de84903cd708f626b8620220628e58adbb1e2f
Eugene Shevchenko <acc+github@takogo.net>
2021-11-11 18:13:46 +0300

    page: 010 basic app

--- examples/nodejs/010_basic_app/app.js
+++ examples/nodejs/010_basic_app/app.js
@@ -3,8 +3,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// [<snippet ping-route-import>]
 const indexRouter = require('./routes/index');
 const pingRouter = require('./routes/ping');
+// [<endsnippet ping-route-import>]
 
 const app = express();
 
@@ -14,7 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// [<snippet ping-route-middleware>]
 app.use('/', indexRouter);
 app.use('/ping', pingRouter);
+// [<endsnippet ping-route-middleware>]
 
 module.exports = app;
