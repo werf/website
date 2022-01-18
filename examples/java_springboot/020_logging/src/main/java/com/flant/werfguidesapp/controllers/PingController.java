aa04ca43d54e5eef56b14f0111eb4ab68de0e3b9
Konstantin Nezhbert <zhbert@yandex.ru>
2022-01-18 13:25:36 +0300

    Add article text

--- examples/java_springboot/020_logging/src/main/java/com/flant/werfguidesapp/controllers/PingController.java
+++ examples/java_springboot/020_logging/src/main/java/com/flant/werfguidesapp/controllers/PingController.java
@@ -1,21 +1,16 @@
 package com.flant.werfguidesapp.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
 public class PingController {
 
-	private Logger log = LoggerFactory.getLogger(this.getClass());
-
 	@GetMapping("/ping")
 	public String Ping(HttpServletRequest httpServletRequest) {
-		log.info("Endpoint /ping request from the {} address", httpServletRequest.getRemoteAddr());
 		return "pong\n";
 	}
 }
--- pages_ru/guides/java_springboot/200_real_apps/020_logging.md
+++ pages_ru/guides/java_springboot/200_real_apps/020_logging.md
@@ -1,6 +1,5 @@
 ---
 title: Логирование
-layout: guides-wip
 permalink: guides/java_springboot/200_real_apps/020_logging.html
 examples_initial: examples/java_springboot/010_basic_app
 examples: examples/java_springboot/020_logging
