584094d8724a3d3e844d3c40ac149c8677cee97b
Artem Kosulin <artem.kosulin@flant.com>
2023-04-19 14:56:21 +0300

    Fix configurator and tab scripts
    Signed-off-by: Artem Kosulin <artem.kosulin@flant.com>

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -16,26 +16,26 @@ function openTab(evt, linksClass, contentClass, contentId) {
 }
 
 // TODO: refactor this code
-// $(document).ready(() => {
-//   const $buttons = $('[data-tabs-button]');
-//   let fileName = '';
-//
-//   let player = AsciinemaPlayer.create(`/assets/videos/build.cast`, document.getElementById('demo'), {
-//     fit: "both",
-//     autoplay: true,
-//   });
-//
-//   $buttons.each((idx, button) => {
-//     $(button).click(() => {
-//       $buttons.each((i, button) => $(button).removeClass('active'))
-//       $(button).addClass('active');
-//       fileName = $(button).attr('data-tabs-button');
-//       player.dispose();
-//
-//       player = AsciinemaPlayer.create(`/assets/videos/${fileName}.cast`, document.getElementById('demo'), {
-//         fit: "both",
-//         autoplay: true,
-//       });
-//     })
-//   })
-// })
+$(document).ready(() => {
+  const $buttons = $('[data-tabs-button]');
+  let fileName = '';
+
+  let player = AsciinemaPlayer.create(`/assets/videos/build.cast`, document.getElementById('demo'), {
+    fit: "both",
+    autoplay: true,
+  });
+
+  $buttons.each((idx, button) => {
+    $(button).click(() => {
+      $buttons.each((i, button) => $(button).removeClass('active'))
+      $(button).addClass('active');
+      fileName = $(button).attr('data-tabs-button');
+      player.dispose();
+
+      player = AsciinemaPlayer.create(`/assets/videos/${fileName}.cast`, document.getElementById('demo'), {
+        fit: "both",
+        autoplay: true,
+      });
+    })
+  })
+})
