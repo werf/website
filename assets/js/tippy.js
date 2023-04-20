3913aa8791fb9d8a79788a858b1070e8d2e50dc9
Artem Kosulin <artem.kosulin@flant.com>
2023-04-20 22:24:21 +0300

    Add tippy in documentation
    Signed-off-by: Artem Kosulin <artem.kosulin@flant.com>

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -21,14 +21,35 @@ document.addEventListener('DOMContentLoaded', () => {
     delay: [0, 200],
   });
 
-  tippy('span.tooltip-text', {
+  tippy('[data-tooltip-content]', {
     content(reference) {
-      const title = reference.getAttribute('title');
-      reference.removeAttribute('title');
+      const title = reference.getAttribute('data-tooltip-content');
+      reference.removeAttribute('data-tooltip-content');
       return title;
     },
+    theme: 'werf',
+    sticky: true,
+    hideOnClick: false,
+    showOnCreate: true,
+    trigger: 'manual',
     allowHTML: true,
-    interactive: true,
-    delay: [0, 200],
+    zIndex: 4,
+    popperOptions: {
+      strategy: 'fixed',
+      modifiers: [
+        {
+          name: 'flip',
+          options: {
+            fallbackPlacements: [],
+          },
+        },
+        {
+          name: 'preventOverflow',
+          options: {
+            mainAxis: false,
+          },
+        },
+      ],
+    },
   });
 })
