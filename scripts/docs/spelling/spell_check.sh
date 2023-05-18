10b5407539f04a3e5e34fc63a7503f4f79cc187d
Zhbert <konstantin.nezhbert@flant.com>
2023-05-18 11:01:45 +0300

    Add new tasks into Taskfile
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- scripts/docs/spelling/spell_check.sh
+++ scripts/docs/spelling/spell_check.sh
@@ -4,10 +4,15 @@ set -e
 
 arg_site_lang="${1:?ERROR: Site language \'en\' or \'ru\' should be specified as the first argument.}"
 
+if [[ "$arg_site_lang" == "en" ]]; then
+  language="en_US"
+elif [[ "$arg_site_lang" == "ru" ]]; then
+  language="ru_RU"
+fi
+
 script=$(cat <<EOF
 cd /spelling/$arg_site_lang && \
-  hunspell
-EOF
+  echo $language \
 )
 
-werf run doc_$arg_site_lang --docker-options="--entrypoint=bash" -- -c "$script"
+werf run spell-checker --docker-options="--entrypoint=bash" -- -c "$script"
