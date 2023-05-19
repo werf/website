7e230409b8eb11ce1d7dcdfa1ff486014a338065
Timofey Kirillov <timofey.kirillov@flant.com>
2023-05-19 17:23:00 +0000

    chore(demo): re-record comparison demos (2)
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- scripts/demo/run_failed_deploy.sh
+++ scripts/demo/run_failed_deploy.sh
@@ -2,6 +2,6 @@
 
 demoscriptsdir="$(realpath $(dirname -- "$BASH_SOURCE"))"
 
-tmux new-session  -d "source ${demoscriptsdir}/env.sh ; cd ${WERF_DIR} ; INSTANT_TYPE=1 TYPE_DELAY=1 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source ${demoscriptsdir}/_type_and_execute.sh ${demoscriptsdir}/raw_scenario/failed_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
-     split-window -h -l 65% "source ${demoscriptsdir}/env.sh ; cd ${WERF_DIR} ; WERF_LOG_TERMINAL_WIDTH=90 WERF_REQUIRE_BUILT_IMAGES=1 INSTANT_TYPE=1 TYPE_DELAY=1 source ${demoscriptsdir}/_type_and_execute.sh ${demoscriptsdir}/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
+tmux new-session  -d "source ${demoscriptsdir}/env.sh ; cd ${WERF_DIR} ; TYPE_DELAY=1 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source ${demoscriptsdir}/_type_and_execute.sh ${demoscriptsdir}/raw_scenario/failed_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
+     split-window -h -l 65% "source ${demoscriptsdir}/env.sh ; cd ${WERF_DIR} ; WERF_LOG_TERMINAL_WIDTH=90 WERF_REQUIRE_BUILT_IMAGES=1 TYPE_DELAY=1 source ${demoscriptsdir}/_type_and_execute.sh ${demoscriptsdir}/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
      attach
