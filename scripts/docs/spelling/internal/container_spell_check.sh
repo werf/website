1f63be15efac0251406e8e45cdf3f46c9ea9dd7a
Zhbert <konstantin.nezhbert@flant.com>
2023-05-19 14:30:39 +0300

    Add GitHub Action
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- scripts/docs/spelling/internal/container_spell_check.sh
+++ scripts/docs/spelling/internal/container_spell_check.sh
@@ -3,15 +3,20 @@
 set -e
 
 arg_site_lang="${1:?ERROR: Site language \'en\' or \'ru\' should be specified as the first argument.}"
+str=$'\n'
 
 if [[ "$arg_site_lang" == "en" ]]; then
   language="en_US"
 elif [[ "$arg_site_lang" == "ru" ]]; then
-  language="ru_RU"
+  language="ru_RU,en_US"
 fi
 
-for file in `find ./ -type f -name "*.html"`
-do
-   wc -l $file;
-   stat -c %s $file;
-done
+html2text 404.html | hunspell -d $language -l
+
+#for file in `find ./ -type f -name "*.html"`
+#do
+#  echo "$str"
+#  echo "Checking $file..."
+#  cat $file
+#  html2text $file | hunspell -d $language -l
+#done
--- scripts/docs/spelling/spell_check.sh
+++ scripts/docs/spelling/spell_check.sh
@@ -4,12 +4,6 @@ set -e
 
 arg_site_lang="${1:?ERROR: Site language \'en\' or \'ru\' should be specified as the first argument.}"
 
-if [[ "$arg_site_lang" == "en" ]]; then
-  language="en_US"
-elif [[ "$arg_site_lang" == "ru" ]]; then
-  language="ru_RU"
-fi
-
 script=$(cat <<EOF
 cd /spelling/$arg_site_lang && \
   ./container_spell_check.sh $arg_site_lang
