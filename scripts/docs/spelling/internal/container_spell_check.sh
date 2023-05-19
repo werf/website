7d6a86a6e2b3636fee6d979b34c620df9b19384d
Zhbert <konstantin.nezhbert@flant.com>
2023-05-19 14:53:04 +0300

    Solve encoding problems
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- scripts/docs/spelling/internal/container_spell_check.sh
+++ scripts/docs/spelling/internal/container_spell_check.sh
@@ -9,15 +9,20 @@ if [[ "$arg_site_lang" == "en" ]]; then
   language="en_US"
 elif [[ "$arg_site_lang" == "ru" ]]; then
   language="ru_RU,en_US"
+
+  cp /usr/share/hunspell/en_US.aff  /usr/share/hunspell/en_US.aff.orig
+  cp /usr/share/hunspell/en_US.dic  /usr/share/hunspell/en_US.dic.orig
+  iconv --from ISO8859-1 /usr/share/hunspell/en_US.aff.orig > /usr/share/hunspell/en_US.aff
+  iconv --from ISO8859-1 /usr/share/hunspell/en_US.dic.orig > /usr/share/hunspell/en_US.dic
+  sed -i 's/SET ISO8859-1/SET UTF-8/' /usr/share/hunspell/en_US.aff
 fi
 
 echo "Checking $arg_site_lang docs..."
-html2text 404.html | hunspell -d $language -l
+html2text -utf8 404.html | sed '/^$/d' | hunspell -d $language -l
 
-#for file in `find ./ -type f -name "*.html"`
-#do
-#  echo "$str"
-#  echo "Checking $file..."
-#  cat $file
-#  html2text $file | hunspell -d $language -l
-#done
+for file in `find ./ -type f -name "*.html"`
+do
+  echo "$str"
+  echo "Checking $file..."
+  html2text -utf8 $file | sed '/^$/d' | hunspell -d $language -l
+done
