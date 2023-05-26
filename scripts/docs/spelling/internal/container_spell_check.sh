388c944b679d8a94f5cbdca0a236a4a241e7d2b6
Zhbert <konstantin.nezhbert@flant.com>
2023-05-26 15:20:23 +0300

    Add task commands and custom dict generator
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- scripts/docs/spelling/internal/container_spell_check.sh
+++ scripts/docs/spelling/internal/container_spell_check.sh
@@ -6,15 +6,17 @@ arg_site_lang="${1:?ERROR: Site language \'en\' or \'ru\' should be specified as
 str=$'\n'
 
 if [[ "$arg_site_lang" == "en" ]]; then
-  language="en_US"
+  language="en_US,dev_OPS"
 elif [[ "$arg_site_lang" == "ru" ]]; then
-  language="ru_RU,en_US"
+  language="ru_RU,en_US,dev_OPS"
 fi
 
 cp /usr/share/hunspell/en_US.aff  /usr/share/hunspell/en_US.aff.orig
 cp /usr/share/hunspell/en_US.dic  /usr/share/hunspell/en_US.dic.orig
 iconv --from ISO8859-1 /usr/share/hunspell/en_US.aff.orig > /usr/share/hunspell/en_US.aff
 iconv --from ISO8859-1 /usr/share/hunspell/en_US.dic.orig > /usr/share/hunspell/en_US.dic
+rm /usr/share/hunspell/en_US.aff.orig
+rm /usr/share/hunspell/en_US.dic.orig
 sed -i 's/SET ISO8859-1/SET UTF-8/' /usr/share/hunspell/en_US.aff
 
 echo "Checking $arg_site_lang docs..."
