cdb7890331d42b85c3f6242d9d90b1912ac136da
Zhbert <konstantin.nezhbert@flant.com>
2023-06-13 15:51:51 +0300

    Add the plain text display task command for HTML
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- scripts/docs/spelling/spell_check.sh
+++ scripts/docs/spelling/spell_check.sh
@@ -8,9 +8,13 @@ if [ -n "$2" ]; then
   arg_target_page=$2
 fi
 
+if [ -n "$3" ]; then
+  arg_get_plain_text=$3
+fi
+
 script=$(cat <<EOF
 cd /spelling/$arg_site_lang && \
-  ./container_spell_check.sh $arg_site_lang $arg_target_page
+  ./container_spell_check.sh $arg_site_lang $arg_target_page $arg_get_plain_text
 EOF
 )
 
