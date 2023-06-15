bd1337f7852156b1d94af277fb6f747a1a5930d7
Zhbert <konstantin.nezhbert@flant.com>
2023-06-15 14:29:50 +0300

    Fix pong
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- examples/golang/010_basic_app/internal/app/app.go
+++ examples/golang/010_basic_app/internal/app/app.go
@@ -10,7 +10,7 @@ func Run() {
 	route := gin.Default()
 
 	route.GET("/ping", func(context *gin.Context) {
-		context.String(http.StatusOK, "pong")
+		context.String(http.StatusOK, "pong\n")
 	})
 
 	err := route.Run()
