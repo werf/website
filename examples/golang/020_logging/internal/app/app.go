c03b59a369630a72705dd91a45c7f0a5ccaf01ff
Zhbert <konstantin.nezhbert@flant.com>
2023-06-16 15:30:48 +0300

    Add information about log levels
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- examples/golang/020_logging/internal/app/app.go
+++ examples/golang/020_logging/internal/app/app.go
@@ -8,9 +8,11 @@ import (
 )
 
 func Run() {
+	// [<snippet log_enable>]
 	route := gin.New()
 	route.Use(gin.Recovery())
 	route.Use(common.JsonLogger())
+	// [<endsnippet log_enable>]
 
 	route.GET("/ping", func(context *gin.Context) {
 		context.String(http.StatusOK, "pong\n")
