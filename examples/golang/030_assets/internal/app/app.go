eff35b5314be64173b40c83b3f5ebaa19a5bb579
Zhbert <konstantin.nezhbert@flant.com>
2023-06-19 13:45:43 +0300

    Add new image endpoints
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- examples/golang/030_assets/internal/app/app.go
+++ examples/golang/030_assets/internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"030_assets/internal/common"
+	"030_assets/internal/controllers"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -14,9 +15,17 @@ func Run() {
 	route.Use(common.JsonLogger())
 	// [<endsnippet log_enable>]
 
+	route.Static("/static/stylesheets", "static/stylesheets")
+	route.Static("/static/javascripts", "static/javascripts")
+	route.Static("/static/images", "static/images")
+
+	route.LoadHTMLGlob("templates/*")
+
 	route.GET("/ping", func(context *gin.Context) {
 		context.String(http.StatusOK, "pong\n")
 	})
+	route.GET("/", controllers.MainPage)
+	route.GET("/image", controllers.ImagePage)
 
 	err := route.Run()
 	if err != nil {
