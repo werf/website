8c64b1de65a75a0a04bedd906d6d115a8aea6b62
Polina Sizintseva <polina.sizintseva@flant.com>
2026-01-14 11:59:57 +0300

    chore(docs): ignore changelog (#835)

--- scripts/docs/check_broken_links.sh
+++ scripts/docs/check_broken_links.sh
@@ -10,7 +10,7 @@ cd /app && \
     --allow-hash-href \
     --empty-alt-ignore \
     --check_html \
-    --file_ignore "/____________/" \
+    --file_ignore "/____________/,/changelog.html/" \
     --url_ignore "/localhost/,/cloudyuga.guru/,/t.me/,/slack.com/,/cncf.io/,/example.com/,/github.com/werf/,/habr.com/,/apple-touch-icon.png/,/site.webmanifest/,/favicon/,/safari-pinned-tab/,/docs/,/guides.html/" \
     --url-swap "github.com/werf/website/blob/main:github.com/werf/website/blob/$GITHUB_SHA,github.com/werf/website/tree/main:github.com/werf/website/tree/$GITHUB_SHA" \
     --http-status-ignore "0,429,403" \
